904415c955b1a4e1333d2dc5300cd7075bae8cee
jenny-morin <jennifer.l.morin@maine.edu>
2026-02-26 15:29:29 -0500

    Jennys commit to the app

--- cos420-fridgefriend/src/App.tsx
+++ cos420-fridgefriend/src/App.tsx
@@ -14,6 +14,9 @@ function App() {
           <li>
             Isabel Layland
           </li>
+          <li>
+            Jennifer Morin
+          </li>
         </ul>
       </header>
     </div>
