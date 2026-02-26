b8560f6642f06b278fe51e1f05d3bc42a1dd72e0
AppreciativeWalrus <appreciativewalrus@gmail.com>
2026-02-26 15:24:22 -0500

    React template setup.

---
+++ cos420-fridgefriend/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          FRIDGE FRIEND!!!
+        </p>
+        <h1>Contributors</h1>
+        <ul>
+          <li>
+            Isabel Layland
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
