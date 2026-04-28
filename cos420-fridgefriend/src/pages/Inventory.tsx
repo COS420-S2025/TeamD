b9f4dfc911c125145d6afbb9b575caafb203b90f
AppreciativeWalrus <appreciativewalrus@gmail.com>
2026-04-28 14:59:35 -0400

    Inventory page now has sorting and filters! And shows all item info!

--- cos420-fridgefriend/src/pages/Inventory.tsx
+++ cos420-fridgefriend/src/pages/Inventory.tsx
@@ -12,7 +12,7 @@ interface data {
   opened: boolean;
   price: number;
   quantity: number;
-  expiry: string
+  expiry: Date;
 }
 
 // function properties(){
@@ -20,10 +20,73 @@ interface data {
 // }
 function ItemList(){
   //const[foodItem, setFoodItem]= useState(null);
-  const [foodItem, setFoodItem] = useState<any>(null);
+  const [foodItem, setFoodItem] = useState<data[]>([]);
   const [Inventory, setInventory] = useState<any>(null);
   const itemCollRef = collection(db, 'foodItem');
   
+  // Put sort modes can be changed by chaning mode. Need to make more functions for it
+  // 0 = expiry
+  // 1 = location
+  function SortList(mode: number){
+
+    if(mode == -1)
+      setInventory(foodItem.map(item =>
+        MakeListItem(item)
+      ));
+
+    if(mode == 0)
+      setInventory(foodItem.sort((a, b) => SortByExpiry(a,b))
+        .map(item =>
+        MakeListItem(item)
+      ));
+
+    if(mode == 1)
+      setInventory(foodItem.sort((a, b) => SortByLocation(a,b))
+        .map(item =>
+        MakeListItem(item)
+      ));
+
+      if(mode == 2)
+      setInventory(foodItem.sort((a, b) => SortByPrice(a,b))
+        .map(item =>
+        MakeListItem(item)
+      ));
+  }
+
+  function SortByExpiry(a: data, b:data): number{
+    if(a.expiry.getTime() > b.expiry.getTime()) return 1;
+    if(a.expiry.getTime() < b.expiry.getTime()) return -1;
+    return 0;
+  }
+
+  function SortByPrice(a: data, b:data): number{
+    if(a.price > b.price) return 1;
+    if(a.price < b.price) return -1;
+    return 0;
+  }
+
+  function SortByLocation(a: data, b:data): number{
+    return a.location.localeCompare(b.location);
+  }
+
+  function SortByCategory(a: data, b:data): number{
+    return a.category.localeCompare(b.category);
+  }
+
+  function FilterByLocation(locale: string){
+
+    setInventory(foodItem.filter((a) => a.location.indexOf(locale) != -1)
+        .map(item =>
+        MakeListItem(item)
+      ));
+  }
+
+    function FilterByCategory(cat: string){
+    setInventory(foodItem.filter((a) => a.category == cat)
+        .map(item =>
+        MakeListItem(item)
+      ));
+  }
   
   return (
     <>
@@ -40,33 +103,40 @@ function ItemList(){
           </nav>
           <Outlet />
           <h2 className="header-bar">Inventory</h2>
-          <select>
-              <option id="fridge" value="Fridge">Fridge</option>
-              <option id="fridge" value="Freezer">Freezer</option>
-              <option id="fridge" value="Pantry">Pantry</option>
+          <select onChange={e => FilterByLocation(e.target.value)}>
+              <option id="location-any" value="">Any</option>
+              <option id="location-fridge" value="Fridge">Fridge</option>
+              <option id="location-freezer" value="Freezer">Freezer</option>
+              <option id="location-pantry" value="Pantry">Pantry</option>
           </select>
           
           <button onClick = {async () =>{
             const collData = await getDocs(itemCollRef);
-            const itemArray = collData.docs.map(doc => ({ 
+            const itemArray: data[] = collData.docs.map(doc => ({ 
               itemName: doc.data().itemName,
               category: doc.data().category,
               location: doc.data().location,
               opened: doc.data().opened,
               price: doc.data().price,
               quantity: doc.data().quantity,
-              expiry: doc.data().expiry
-
-            }));
-            console.log(itemArray)
-            setFoodItem(itemArray)
-            console.log(foodItem)
+              expiry: new Date(Date.parse(doc.data().expiry))
+            } as data));
+            console.log(itemArray);
+            setFoodItem(itemArray);
+            console.log(foodItem);
             const listItems = itemArray.map(item =>
-            MakeListItem(item)
+              MakeListItem(item)
             );
             
-          setInventory(listItems)
+          setInventory(listItems);
           }}>Load Items</button>
+          <br></br>
+          <select onChange={e => SortList(parseInt(e.target.value))}>
+              <option id="sortBy-any" value="-1">No Sort</option>
+              <option id="sortBy-location" value="1">location</option>
+              <option id="sortBy-expiry" value="0">expiry</option>
+              <option id="sortBy-price" value="2">price</option>
+          </select>
           
           {foodItem == null ? <p>Loading</p>: <ul>{Inventory}</ul>}
           <Link to="/inventory/addItem"><button>Add</button></Link>
@@ -86,40 +156,17 @@ function MakeListItem(item: data)
         <p>
           <b>{item.itemName}:</b>
           <div>{' Location: '+ item.location}</div>
-          
+          <div> Category: {item.category}</div>
+          <div> Expires: {item.expiry.toDateString()}</div>
+          <div> Opened: {item.opened?"Opened":"Closed"}</div>
+          <div> Price: {item.price}</div>
           <div> Amount: {item.quantity}</div>
         </p>
       </li>
     )
 }
 
-// Put sort modes can be changed by chaning mode. Need to make more functions for it
-// 0 = expiry
-// 1 = location
-function SortList(mode: number){
-  if(mode == 0)
-    setListItems(dummyList.sort((a, b) => SortByExpiry(a,b))
-      .map(item =>
-      MakeListItem(item)
-    ));
 
-    if(mode == 1)
-    setListItems(dummyList.sort((a, b) => SortByLocation(a,b))
-      .map(item =>
-      MakeListItem(item)
-    ));
-}
-
-function SortByExpiry(a: data, b:data): number{
-  if(!a.expiry || !b.expiry) return 0;
-  if(a.expiry.getTime() > b.expiry.getTime()) return 1;
-  if(a.expiry.getTime() > b.expiry.getTime()) return -1;
-  return 0;
-}
-
-function SortByLocation(a: data, b:data): number{
-  return a.location.localeCompare(b.location);
-}
 
 // const Inventory = () => {
 //   return (
